dce3c1d527fdb13798400a19123b195ebd5599d6
website-f <mohdfitri1017@gmail.com>
2025-05-07 10:12:40 +0800

    add

--- resources/views/kiosk.blade.php
+++ resources/views/kiosk.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>McDonald's Kiosk UI</title>
+    <title>QBOT</title>
     <link rel="stylesheet" href="{{ asset('newMain/css/main.css')}}" />
     <link rel="stylesheet" href="{{ asset('newMain/css/home.css')}}" />
     <link rel="stylesheet" href="{{ asset('newMain/css/menu.css')}}" />
